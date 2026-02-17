299b00a9fa1b3be0822ddaf5c894f940439f55ae
Anton <anton@example.com>
2026-02-17 23:39:45 +0100

    network reute example

--- hosts/korsar/network.nix
+++ hosts/korsar/network.nix
@@ -14,5 +14,10 @@ in
     xiaomi5g = mkWifi "Xiaomi_5G";
     xiaomi24 = mkWifi "Xiaomi";
   };
+
+#  networking.routes = [
+#    { address = "10.0.0.0"; prefixLength = 8; via = "10.10.10.195"; }
+#    { address = "172.16.0.0"; prefixLength = 12; via = "10.10.10.195"; }
+#  ];
 }
 
