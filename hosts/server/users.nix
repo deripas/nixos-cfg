c74eca57c71685b40281afcb4eed5c7bb033c4e9
Anton <deripas@yandex.ru>
2026-08-01 22:15:50 +0200

    add pub key

--- hosts/server/users.nix
+++ hosts/server/users.nix
@@ -21,5 +21,9 @@
       "podman"
       "libvirtd"
     ];
+
+    openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHhvgx4InwRr8sWtI5pkogCh/AJsS4fwBDOhftTYK7Re"
+    ];
   };
 }
