ee9ee9c363cb7ae701412e632c98acb80a776d04
Anton <deripas@yandex.ru>
2026-09-07 00:37:50 +0200

    enable b2 backup

--- hosts/server/services/immich.nix
+++ hosts/server/services/immich.nix
@@ -116,11 +116,11 @@ in
     initialize = true;
 
     # Автоматический запуск (после дампа Postgres)
-    timerConfig = null;
-    #timerConfig = {
-    #  OnCalendar = "*-*-* 02:00:00";
-    #  Persistent = true;
-    #};
+    #timerConfig = null;
+    timerConfig = {
+      OnCalendar = "*-*-* 02:00:00";
+      Persistent = true;
+    };
 
     # Подключение к Backblaze B2 через S3 API
     repository = "s3:s3.eu-central-003.backblazeb2.com/deripas-immich-backup";
