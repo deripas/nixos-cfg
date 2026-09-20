95c68aead3af337fa0777b16f22fdd5f1ee98790
Anton <deripas@yandex.ru>
2026-09-21 01:16:11 +0200

    ollama

--- hosts/server/services/ollama.nix
+++ hosts/server/services/ollama.nix
@@ -1,16 +1,18 @@
 { pkgs, ... }:
 
 {
-  #networking.firewall.allowedTCPPorts = [ 3389 ];
-
   services.ollama = {
     enable = true;
     port = 11434;
+    host = "0.0.0.0";
+    openFirewall = true;
     package = pkgs.ollama-cuda;
   };
 
   services.open-webui = {
     enable = true;
     port = 11000;
+    host = "0.0.0.0";
+    openFirewall = true;
   };
 }
